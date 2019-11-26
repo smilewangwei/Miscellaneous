e9ccdc7a4683712561859c2af13f2a5eacba5516
smilewangwei <vipres@qq.com>
2019-11-26 10:44:03 +0800

    第二次更改

--- sql题.py
+++ sql题.py
@@ -14,7 +14,7 @@ def main():
     url='https://www.pearvideo.com/category_59'
     response=requests.get(url,headers=headers,timeout=10)
     res_xpath=etree.HTML(response.text) #第一步给内容转为xpath结构
-    title1=res_xpath.xpath('/html/head/title/text()')
+    # title1=res_xpath.xpath('/html/head/title/text()')
     title2=res_xpath.xpath('//title/text()')  #找到title输出文本
     print(title2)
 if __name__ == "__main__":
